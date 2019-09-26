b462907a5e3c4d07c9235db4d433a8921b2b4671
Aakash Agarwal <aakashagarwal6898@gmail.com>
2019-09-27 00:57:20 +0530

    Add files via upload

---
+++ scripts/remove_neg_values.py
@@ -0,0 +1,17 @@
+﻿import os
+import re
+
+
+yourpath = r"E:\Dataset\Dataset_tiny_final\text_files"   # target folder with images 
+for root, dirs, files in os.walk(yourpath, topdown=False):
+    for filename in files:
+
+        txt_file = open(os.path.join(root,filename),"r")
+        new_txt = open(filename,"w")
+        for txt_line in txt_file:
+            txt_line=txt_line.replace("-","")
+            new_txt.write(txt_line)
+        txt_file.close()
+        new_txt.close()
+        break
+            
